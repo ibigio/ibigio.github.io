e297909de74acd05b3c3ef590046b0aba7af2073
Ilan Bigio <ilanbigio@gmail.com>
2020-06-05 12:13:55 -0500

    Deploy Next.js to master

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bee04bc159f2294d3aef";
+/******/ 	var hotCurrentHash = "643efd00a2346ea0837a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
