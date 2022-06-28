19bfd20fda1ed500790a1a4e8c29b792f356ab6d
Ilan Bigio <ilanbigio@gmail.com>
2022-06-28 16:43:51 +0200

    Deploy Next.js to master

---
+++ out/_next/static/qeyexLz7bGJXcVkA7U7EG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a,b){return {"/":[a,b],"/index":[a,b],"/projects/[id]":[a,"static\u002Fcss\u002F8ab69c190193921b911e.css"]}}("static\u002Fchunks\u002F2b7549f8646bdd1c821704fb82cebd4cdac57a0d.bc8fd910ebf419209c9e.js","static\u002Fcss\u002F3fb503e8183920e42120.css"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
