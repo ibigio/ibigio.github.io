854d7826892836cad1a5842ab6381bf5eda88cad
Ilan Bigio <ilanbigio@gmail.com>
2026-01-02 18:14:03 -0500

    Migrate site from Next.js to static HTML

--- components/CopyrightSection.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/CopyrightSection.module.css";
-
-export default function ExperienceSection() {
-  return (
-    <div className={styles.copyright}>
-      © {new Date().getFullYear()} Ilan Bigio
-    </div>
-  );
-}
